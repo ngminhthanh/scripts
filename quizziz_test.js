14f98f977870740972f5ad1eb6d7843ddeea8f68
ngminhthanh <94471046+ngminhthanh@users.noreply.github.com>
2022-04-01 18:43:44 +0700

    Create quizziz_test.js

---
+++ quizziz_test.js
@@ -0,0 +1,391 @@
+
+'use strict';
+/** @type {!Array} */
+var a0_0x46d9 = ["map", "log", "done", "previousCo", "Generator ", "M1-_ZY", "584734QBvHKy", "retreive q", "Enter othe", "$$state", "omaticcly ", "pt created", "throw", "children", "optionData", "pop", "structure", "5169PznWPv", "Couldn't r", "eCAD63NH-x", "n/json", "value", "ioner-comp", "actualInde", "he must ta", "12684cpmDJj", "no-cache", "_data", "applicatio", "etreive qu", "598135XaHJDn", "> div > di", "109oybLnd", "length", "1UDxwwi", " div.scree", "prototype", "apply", "1VuocNV", "v.options-",
+"ke a part ", "Wof4elPFwc", "13063dQNSTt", "join", "oot object", "131XFQUpt", "stion", "strict-ori", "filter", "ext", "body > div", "/github.co", "ops", "5242ymIQpO", "playerId", "random", "opacity", "v > div > ", "t-componen", "tor", "options", "estionID o", "text", "roomHash", " > div.roo", "cors", "__vue__", "container ", "1715117BCiHcW", "push", "call", "empty", "return", "stringify", "include", "Response:", "forEach", "data", "ross-origi", "trys", "same-site", "ame > div.", "-container", "q=0.7,pl;q",
+"color: red", "en-GB,en-U", "1zDUtZG", "retreive r", "MSQ", "detect aut", "ski#5119! ", "Unable to ", "%c \n  Scri", "ame here, ", " executing", "ot defined", "next", "sent", "yer", "_vm", "some", "slice", "t > div > ", "\n  https:/", "live", "main_main", "etreive pr", "innerText", "PlayerID n", "original", "> div", "20%", "parse", "question", "https://qu", "> div.page", "game", "POST", "currentPla", ".com/play-", "label", "function", "querySelec", "json", "r player n", "anwser typ", "n.screen-g",
+"DaB6c-M4tn", "number"];
+/**
+ * @param {number} totalExpectedResults
+ * @param {?} entrySelector
+ * @return {?}
+ */
+var a0_0x3a6b = function(totalExpectedResults, entrySelector) {
+  /** @type {number} */
+  totalExpectedResults = totalExpectedResults - 353;
+  var _0x46d985 = a0_0x46d9[totalExpectedResults];
+  return _0x46d985;
+};
+(function(data, oldPassword) {
+  /** @type {function(number, ?): ?} */
+  var toMonths = a0_0x3a6b;
+  for (; !![];) {
+    try {
+      /** @type {number} */
+      var userPsd = -parseInt(toMonths(403)) * parseInt(toMonths(407)) + parseInt(toMonths(390)) * parseInt(toMonths(405)) + -parseInt(toMonths(415)) + -parseInt(toMonths(426)) * parseInt(toMonths(418)) + -parseInt(toMonths(398)) + parseInt(toMonths(379)) * -parseInt(toMonths(459)) + -parseInt(toMonths(411)) * -parseInt(toMonths(441));
+      if (userPsd === oldPassword) {
+        break;
+      } else {
+        data["push"](data["shift"]());
+      }
+    } catch (_0x123298) {
+      data["push"](data["shift"]());
+    }
+  }
+})(a0_0x46d9, 383220), (() => {
+  /**
+   * @param {!Object} evt
+   * @param {number} fn
+   * @param {!Function} P
+   * @param {!Function} current
+   * @return {?}
+   */
+  var start = function(evt, fn, P, current) {
+    return new (P || (P = Promise))(function(a, reject) {
+      /**
+       * @param {?} value
+       * @return {undefined}
+       */
+      function success(value) {
+        /** @type {function(number, ?): ?} */
+        var parseInt = a0_0x3a6b;
+        try {
+          step(current[parseInt(469)](value));
+        } catch (createConnectionErr) {
+          reject(createConnectionErr);
+        }
+      }
+      /**
+       * @param {?} value
+       * @return {undefined}
+       */
+      function error(value) {
+        /** @type {function(number, ?): ?} */
+        var parseInt = a0_0x3a6b;
+        try {
+          step(current[parseInt(385)](value));
+        } catch (createConnectionErr) {
+          reject(createConnectionErr);
+        }
+      }
+      /**
+       * @param {!Object} data
+       * @return {undefined}
+       */
+      function step(data) {
+        /** @type {function(number, ?): ?} */
+        var random = a0_0x3a6b;
+        var x;
+        if (data["done"]) {
+          a(data[random(394)]);
+        } else {
+          (x = data[random(394)], x instanceof P ? x : new P(function(resolve) {
+            resolve(x);
+          }))["then"](success, error);
+        }
+      }
+      /** @type {function(number, ?): ?} */
+      var parseInt = a0_0x3a6b;
+      step((current = current[parseInt(410)](evt, fn || []))["next"]());
+    });
+  };
+  /**
+   * @param {?} element
+   * @param {!Function} renderers
+   * @return {?}
+   */
+  var polyfill = function(element, renderers) {
+    /**
+     * @param {number} data
+     * @return {?}
+     */
+    function verb(data) {
+      return function(canCreateDiscussions) {
+        return function(obj) {
+          /** @type {function(number, ?): ?} */
+          var getTemplatesKey = a0_0x3a6b;
+          if (tag) {
+            throw new TypeError(getTemplatesKey(377) + "is already" + getTemplatesKey(467) + ".");
+          }
+          for (; item;) {
+            try {
+              if (tag = 1, data && (id = 2 & obj[0] ? data[getTemplatesKey(445)] : obj[0] ? data[getTemplatesKey(385)] || ((id = data[getTemplatesKey(445)]) && id[getTemplatesKey(443)](data), 0) : data[getTemplatesKey(469)]) && !(id = id[getTemplatesKey(443)](data, obj[1]))[getTemplatesKey(375)]) {
+                return id;
+              }
+              switch(data = 0, id && (obj = [2 & obj[0], id[getTemplatesKey(394)]]), obj[0]) {
+                case 0:
+                case 1:
+                  id = obj;
+                  break;
+                case 4:
+                  return item[getTemplatesKey(364)]++, {
+                    "value" : obj[1],
+                    "done" : false
+                  };
+                case 5:
+                  item[getTemplatesKey(364)]++;
+                  data = obj[1];
+                  /** @type {!Array} */
+                  obj = [0];
+                  continue;
+                case 7:
+                  obj = item["ops"][getTemplatesKey(388)]();
+                  item[getTemplatesKey(452)]["pop"]();
+                  continue;
+                default:
+                  if (!((id = (id = item[getTemplatesKey(452)])[getTemplatesKey(406)] > 0 && id[id["length"] - 1]) || 6 !== obj[0] && 2 !== obj[0])) {
+                    /** @type {number} */
+                    item = 0;
+                    continue;
+                  }
+                  if (3 === obj[0] && (!id || obj[1] > id[0] && obj[1] < id[3])) {
+                    item["label"] = obj[1];
+                    break;
+                  }
+                  if (6 === obj[0] && item[getTemplatesKey(364)] < id[1]) {
+                    item[getTemplatesKey(364)] = id[1];
+                    id = obj;
+                    break;
+                  }
+                  if (id && item["label"] < id[2]) {
+                    item[getTemplatesKey(364)] = id[2];
+                    item[getTemplatesKey(425)][getTemplatesKey(442)](obj);
+                    break;
+                  }
+                  if (id[2]) {
+                    item[getTemplatesKey(425)][getTemplatesKey(388)]();
+                  }
+                  item["trys"][getTemplatesKey(388)]();
+                  continue;
+              }
+              obj = renderers[getTemplatesKey(443)](element, item);
+            } catch (quick) {
+              /** @type {!Array} */
+              obj = [6, quick];
+              /** @type {number} */
+              data = 0;
+            } finally {
+              /** @type {number} */
+              tag = id = 0;
+            }
+          }
+          if (5 & obj[0]) {
+            throw obj[1];
+          }
+          return {
+            "value" : obj[0] ? obj[1] : void 0,
+            "done" : true
+          };
+        }([data, canCreateDiscussions]);
+      };
+    }
+    /** @type {function(number, ?): ?} */
+    var epsilon = a0_0x3a6b;
+    var tag;
+    var data;
+    var id;
+    var g;
+    var item = {
+      "label" : 0,
+      "sent" : function() {
+        if (1 & id[0]) {
+          throw id[1];
+        }
+        return id[1];
+      },
+      "trys" : [],
+      "ops" : []
+    };
+    return g = {
+      "next" : verb(0),
+      "throw" : verb(1),
+      "return" : verb(2)
+    }, epsilon(365) == typeof Symbol && (g[Symbol["iterator"]] = function() {
+      return this;
+    }), g;
+  };
+  /**
+   * @return {?}
+   */
+  var merge = function() {
+    /** @type {function(number, ?): ?} */
+    var wrapChords = a0_0x3a6b;
+    var e1 = document["querySelec" + wrapChords(432)](wrapChords(423) + wrapChords(437) + wrapChords(431) + wrapChords(475) + "div > div " + wrapChords(359) + wrapChords(455) + ".in-quiz >" + wrapChords(408) + wrapChords(370) + wrapChords(454) + "transition" + "er.transit" + wrapChords(395) + "onent > di" + wrapChords(430) + "div > div " + wrapChords(404) + wrapChords(412) + wrapChords(440) + wrapChords(354));
+    if (!e1) {
+      throw new Error(wrapChords(464) + wrapChords(380) + "uestions l" + "ist elemen" + "t");
+    }
+    return e1;
+  };
+  /**
+   * @param {!Object} el
+   * @return {undefined}
+   */
+  var status = function(el) {
+    /** @type {function(number, ?): ?} */
+    var property = a0_0x3a6b;
+    el["style"][property(429)] = property(355);
+  };
+  start(void 0, void 0, void 0, function() {
+    var playerId;
+    var urlData;
+    return polyfill(this, function(canCreateDiscussions) {
+      /** @type {function(number, ?): ?} */
+      var ch = a0_0x3a6b;
+      if (console["log"](ch(465) + ch(384) + " by gbaran" + ch(463) + ch(476) + ch(424) + "m/gbaransk" + "i/quizizz-" + "cheat\n  ", ch(457) + ";"), !(playerId = prompt(ch(381) + ch(368) + ch(466) + ch(397) + ch(413) + "in quiz!"))) {
+        throw new Error(ch(481) + ch(468));
+      }
+      return urlData = function() {
+        /** @type {function(number, ?): ?} */
+        var num = ch;
+        var tiledImageBRs = document[num(366) + "tor"](num(423));
+        if (!tiledImageBRs) {
+          throw new Error("Could not " + num(460) + num(417));
+        }
+        var tiledImageBR = tiledImageBRs[num(439)];
+        var zoneId = tiledImageBR["$store"][num(472)]["currentQue" + num(419)]["id"];
+        var ddform = tiledImageBR["$store"]["_vm"][num(400)][num(382)][num(360)][num(450)][num(436)];
+        if (!zoneId || !ddform) {
+          throw new Error("Couldn't r" + num(402) + num(434) + "r roomHash");
+        }
+        var data = localStorage["getItem"](num(376) + "ntext");
+        if (!data) {
+          throw new Error(num(391) + num(479) + "eviousCont" + num(422));
+        }
+        return {
+          "id" : zoneId,
+          "roomHash" : ddform,
+          "playerId" : JSON[num(356)](data)[num(362) + num(471)][num(427)]
+        };
+      }(), function(data) {
+        start(void 0, void 0, void 0, function() {
+          var t;
+          var tween;
+          return polyfill(this, function(images) {
+            /** @type {function(number, ?): ?} */
+            var wrap = a0_0x3a6b;
+            switch(images[wrap(364)]) {
+              case 0:
+                return [4, fetch("https://ga" + "me.quizizz" + wrap(363) + "api/v4/pro" + "ceedGame", {
+                  "headers" : {
+                    "accept" : wrap(401) + wrap(393),
+                    "accept-language" : wrap(458) + "S;q=0.9,en" + ";q=0.8,no;" + wrap(456) + "=0.6",
+                    "cache-control" : wrap(399),
+                    "content-type" : wrap(401) + wrap(393),
+                    "experiment-name" : wrap(478),
+                    "pragma" : "no-cache",
+                    "sec-fetch-dest" : wrap(444),
+                    "sec-fetch-mode" : wrap(438),
+                    "sec-fetch-site" : wrap(453),
+                    "x-csrf-token" : wrap(392) + wrap(371) + wrap(414) + wrap(378)
+                  },
+                  "referrer" : wrap(358) + "izizz.com/",
+                  "referrerPolicy" : wrap(420) + "gin-when-c" + wrap(451) + "n",
+                  "body" : JSON[wrap(446)](data),
+                  "method" : wrap(361),
+                  "mode" : wrap(438),
+                  "credentials" : wrap(447)
+                })];
+              case 1:
+                return [4, images[wrap(470)]()[wrap(367)]()];
+              case 2:
+                return t = images[wrap(470)](), console[wrap(374)](wrap(448), t), (tween = t["question"][wrap(389)]["answer"]) instanceof Array && tween[wrap(406)] > 0 ? function(params) {
+                  /** @type {function(number, ?): ?} */
+                  var now = wrap;
+                  var parent = merge();
+                  var rpm_traffic = Array[now(409)][now(474)][now(443)](parent["children"]);
+                  var updatedEdgesById = rpm_traffic[now(421)](function(currentItems) {
+                    return params["some"](function(isSlidingUp) {
+                      /** @type {function(number, ?): ?} */
+                      var parseInt = a0_0x3a6b;
+                      return isSlidingUp === currentItems[parseInt(439)]["optionData"][parseInt(396) + "x"];
+                    });
+                  })[now(373)](function(myPreferences) {
+                    /** @type {function(number, ?): ?} */
+                    var getPreferenceKey = now;
+                    return myPreferences[getPreferenceKey(480)];
+                  });
+                  console["log"]({
+                    "matching" : updatedEdgesById
+                  });
+                  var service_obj = rpm_traffic[now(421)](function(s) {
+                    /** @type {function(number, ?): ?} */
+                    var edgeId = now;
+                    return !updatedEdgesById[edgeId(473)](function(index) {
+                      /** @type {function(number, ?): ?} */
+                      var prefixStyle = edgeId;
+                      return s[prefixStyle(480)] === index;
+                    });
+                  });
+                  console[now(374)]({
+                    "notMatchingElements" : service_obj
+                  });
+                  service_obj[now(449)](status);
+                }(tween) : wrap(372) == typeof tween ? (previousTarget = tween, currentTarget = merge(), nodes = Array[wrap(409)][wrap(474)][wrap(443)](currentTarget[wrap(386)]), filePattern = nodes[wrap(421)](function(colData) {
+                  /** @type {function(number, ?): ?} */
+                  var parseInt = wrap;
+                  return colData[parseInt(439)][parseInt(387)][parseInt(396) + "x"] === previousTarget;
+                })[wrap(373)](function(srcElmnt) {
+                  return srcElmnt["innerText"];
+                }), console[wrap(374)]({
+                  "matching" : filePattern
+                }), nodes["filter"](function(element) {
+                  return filePattern["some"](function(attrValue) {
+                    return element["innerText"] != attrValue;
+                  });
+                })["forEach"](status)) : t[wrap(357)][wrap(389)][wrap(433)] ? (previousState = t[wrap(357)][wrap(389)][wrap(433)]["map"](function(myPreferences) {
+                  /** @type {function(number, ?): ?} */
+                  var getPreferenceKey = wrap;
+                  return myPreferences[getPreferenceKey(435)];
+                }), alert(previousState[wrap(416)](" or ")), console[wrap(374)](previousState)) : console["log"](wrap(464) + wrap(462) + wrap(383) + wrap(369) + "e", t["question"]), [2];
+            }
+            var previousState;
+            var previousTarget;
+            var currentTarget;
+            var nodes;
+            var filePattern;
+          });
+        });
+      }({
+        "gameType" : ch(477),
+        "playerId" : playerId,
+        "powerupEffects" : {
+          "destroy" : []
+        },
+        "questionId" : urlData["id"],
+        "response" : {
+          "attempt" : 0,
+          "isEvaluated" : false,
+          "questionId" : urlData["id"],
+          "questionType" : ch(461),
+          "provisional" : {
+            "scoreBreakups" : {
+              "correct" : {
+                "base" : 5000,
+                "powerups" : [],
+                "streak" : 20,
+                "timer" : 0,
+                "total" : 500
+              },
+              "incorrect" : {
+                "base" : 0,
+                "powerups" : [],
+                "streak" : 0,
+                "timer" : 0,
+                "total" : 0
+              }
+            },
+            "scores" : {
+              "correct" : 600,
+              "incorrect" : 0
+            },
+            "teamAdjustments" : []
+          },
+          "response" : [],
+          "responseType" : ch(353),
+          "timeTaken" : Math["floor"](1E4 * Math[ch(428)]())
+        },
+        "roomHash" : urlData["roomHash"]
+      }), [2];
+    });
+  });
+})();
